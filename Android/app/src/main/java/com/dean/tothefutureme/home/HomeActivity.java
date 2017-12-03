2a87d6aef68ee3152386a3802edf87e9f826985c
Dean <410082323@qq.com>
2017-12-03 12:37:25 +0800

    Dean
    commit.

---
+++ Android/app/src/main/java/com/dean/tothefutureme/home/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.dean.tothefutureme.home;
+
+import com.dean.android.framework.convenient.activity.ConvenientActivity;
+import com.dean.android.framework.convenient.view.ContentView;
+import com.dean.tothefutureme.R;
+
+/**
+ * HomeActivity
+ * <p>
+ * Created by dean on 2017/12/3.
+ */
+@ContentView(R.layout.activity_home)
+public class HomeActivity extends ConvenientActivity {
+}
--- Android/app/src/main/java/com/dean/tothefutureme/main/MainActivity.java
+++ Android/app/src/main/java/com/dean/tothefutureme/main/MainActivity.java
@@ -1,15 +1,35 @@
 package com.dean.tothefutureme.main;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Intent;
 
+import com.dean.android.framework.convenient.activity.ConvenientMainActivity;
+import com.dean.android.framework.convenient.file.download.listener.FileDownloadListener;
+import com.dean.android.framework.convenient.version.VersionUpdate;
+import com.dean.android.framework.convenient.view.ContentView;
 import com.dean.tothefutureme.R;
+import com.dean.tothefutureme.databinding.ActivityMainBinding;
+import com.dean.tothefutureme.home.HomeActivity;
 
-public class MainActivity extends AppCompatActivity {
+@ContentView(R.layout.activity_main)
+public class MainActivity extends ConvenientMainActivity<ActivityMainBinding> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected boolean isUseDefaultDownloadDialog() {
+        return false;
+    }
+
+    @Override
+    protected FileDownloadListener getFileDownloadListener() {
+        return null;
+    }
+
+    @Override
+    protected void showUpdateDownload(VersionUpdate versionUpdate) {
+
+    }
+
+    @Override
+    protected void closeMainToHomeActivity() {
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
